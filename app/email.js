95b6fe8a801a6e1f42e03c02b35e47bbe442a019
TradeDanielP <tradedanielp@gmail.com>
2024-05-09 00:35:02 -0500

    validacion email y form

--- app/email.js
+++ app/email.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".form").addEventListener("submit", function (event) {
+    document.querySelector(".submit-btn").addEventListener("click", function (event) {
         event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-        console.log("envio form");
-        // Obtiene los valores del formulario
+
+        // Obtén los valores del formulario
         const pickUpLocation = document.getElementById('pickup-location-input').value;
         const dropOffLocation = document.getElementById('dropoff-location-input').value;
         const name = document.getElementById('name').value;
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const tel = document.getElementById('tel').value;
         const pickUpDate = document.getElementById('pickUpDate').value;
         const pickUpTime = document.getElementById('pick-up-time').value;
+        const selectPassengers = document.getElementById('selectPassengers').value;
         const additionalRemarks = document.getElementById('additionalRemarks').value;
         
         // Parámetros para enviar correo al cliente
@@ -20,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             La fecha y hora de la reserva son: ${pickUpDate} y ${pickUpTime}.
             Tu lugar de recogida: ${pickUpLocation}.
             Lugar de destino: ${dropOffLocation}.
+            Recuerda que solo pueden abordar: ${selectPassengers} pasajeros.
             `,
             from_name: 'Vinny Driving Professional Service',
             to_email: email
@@ -31,9 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
             to_name: name,
             message: `
             Se ha realizado una nueva reserva en tu sitio web. 
-            Nombre: ${name}, Correo electrónico: ${email}, Telefono: ${tel},
-            Direccion de recojida: ${pickUpLocation}, Direccion de Destino: ${dropOffLocation},
+            Nombre: ${name}, Correo electrónico: ${email}, Teléfono: ${tel},
+            Dirección de recogida: ${pickUpLocation}, Dirección de Destino: ${dropOffLocation},
             Fecha de Recogida: ${pickUpDate}, Hora de Recogida: ${pickUpTime},
+            Cantidad de Pasajeros: ${selectPassengers},
             Notas adicionales: ${additionalRemarks}
             `
         };
@@ -42,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
         emailjs.send('service_proof', 'template_clientConfirm', paramsCliente)
             .then(function (response) {
                 console.log('Correo al cliente enviado con éxito:', response);
+                alert(`Se ha enviado un correo a su correo ingresado: ${email}`);
             }, function (error) {
                 console.error('Error al enviar el correo al cliente:', error);
             });
@@ -50,9 +54,20 @@ document.addEventListener("DOMContentLoaded", function () {
         emailjs.send('service_proof', 'template_reserve', paramsTuyo)
             .then(function (response) {
                 console.log('Correo a tu dirección enviado con éxito:', response);
+                resetForm(); // Reiniciar el formulario después de enviar los correos
             }, function (error) {
                 console.error('Error al enviar el correo a tu dirección:', error);
             });
-
-    })
+    });
 });
+
+function resetForm() {
+    document.getElementById("pickup-location-input").value = "";
+    document.getElementById("dropoff-location-input").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("tel").value = "";
+    document.getElementById("pickUpDate").value = "";
+    document.getElementById("pick-up-time").value = "";
+    document.getElementById("additionalRemarks").value = "";
+}
