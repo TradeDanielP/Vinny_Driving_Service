4850abea76d65d47d314966540ed9eda011a24d5
TradeDanielP <tradedanielp@gmail.com>
2024-05-29 14:36:13 -0500

    Implementacion de la funcionalidad del correo enviando ambas plantillas a los correos, animaciones aos y alertas swal

--- app/email.js
+++ app/email.js
@@ -1,10 +1,53 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".submit-btn").addEventListener("click", function (event) {
-        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
+    function configureMapElements() {
+        const buttonsClear = document.querySelectorAll(".geocoder-pin-right button");
+        buttonsClear.forEach(button => {
+            button.type = 'button';
+        });
+
+        const buttonReverse = document.querySelector(".directions-icon-reverse");
+        if (buttonReverse) {
+            buttonReverse.type = 'button';
+        }
+
+        const pickUpAddressInput = document.querySelector('#mapbox-directions-origin-input input');
+        if (pickUpAddressInput) {
+            pickUpAddressInput.required = true;
+        }
+
+        const dropOffAddressInput = document.querySelector('#mapbox-directions-destination-input input');
+        if (dropOffAddressInput) {
+            dropOffAddressInput.required = true;
+        }
+    }
+
+    // Observador de mutaciones para detectar cambios en el DOM
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.addedNodes.length) {
+                configureMapElements();
+            }
+        });
+    });
+
+    // Configuración del observador de mutaciones
+    const config = { childList: true, subtree: true };
+    observer.observe(document.getElementById('map'), config);
+
+    document.querySelector("#reserveForm").addEventListener("click", function (event) {
+        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+        
+        // Validar el formulario antes de continuar
+        const form = document.getElementById('form');
+        if (!form.checkValidity()) {
+            form.reportValidity();
+            return; // Detener si el formulario no es válido
+        }
+        
         // Obtén los valores del formulario
-        const pickUpLocation = document.getElementById('pickup-location-input').value;
-        const dropOffLocation = document.getElementById('dropoff-location-input').value;
+        const pickUpAddress = document.querySelector('#mapbox-directions-origin-input input').value;
+        const dropOffAddress = document.querySelector('#mapbox-directions-destination-input input').value;
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const tel = document.getElementById('tel').value;
@@ -12,62 +55,64 @@ document.addEventListener("DOMContentLoaded", function () {
         const pickUpTime = document.getElementById('pick-up-time').value;
         const selectPassengers = document.getElementById('selectPassengers').value;
         const additionalRemarks = document.getElementById('additionalRemarks').value;
-        
+
         // Parámetros para enviar correo al cliente
-        const paramsCliente = {
-            to_name: name,
-            message: `
-            Hola ${name}, gracias por reservar con nosotros. Su reserva ha sido confirmada.
-            La fecha y hora de la reserva son: ${pickUpDate} y ${pickUpTime}.
-            Tu lugar de recogida: ${pickUpLocation}.
-            Lugar de destino: ${dropOffLocation}.
-            Recuerda que solo pueden abordar: ${selectPassengers} pasajeros.
-            `,
-            from_name: 'Vinny Driving Professional Service',
-            to_email: email
+        const paramsClient = {
+            name: name,
+            address1: pickUpAddress,
+            address2: dropOffAddress,
+            date: pickUpDate,
+            time: pickUpTime,
+            passengers: selectPassengers,
+            email: email
         };
 
         // Parámetros para enviar correo a tu dirección de correo electrónico
-        const paramsTuyo = {
-            from_name: 'Vinny Driving Professional Service',
-            to_name: name,
-            message: `
-            Se ha realizado una nueva reserva en tu sitio web. 
-            Nombre: ${name}, Correo electrónico: ${email}, Teléfono: ${tel},
-            Dirección de recogida: ${pickUpLocation}, Dirección de Destino: ${dropOffLocation},
-            Fecha de Recogida: ${pickUpDate}, Hora de Recogida: ${pickUpTime},
-            Cantidad de Pasajeros: ${selectPassengers},
-            Notas adicionales: ${additionalRemarks}
-            `
+        const paramsYours = {
+            name: name,
+            phone: tel,
+            email: email,
+            note: additionalRemarks,
+            address1: pickUpAddress,
+            address2: dropOffAddress,
+            date: pickUpDate,
+            time: pickUpTime,
+            passengers: selectPassengers
         };
-
+  
         // Llamar a la función de EmailJS para enviar el correo al cliente
-        emailjs.send('service_proof', 'template_clientConfirm', paramsCliente)
-            .then(function (response) {
-                console.log('Correo al cliente enviado con éxito:', response);
-                alert(`Se ha enviado un correo a su correo ingresado: ${email}`);
-            }, function (error) {
-                console.error('Error al enviar el correo al cliente:', error);
-            });
+        const sendClientEmail = emailjs.send('service_proof', 'template_clientConfirm', paramsClient);
 
         // Llamar a la función de EmailJS para enviar el correo a tu dirección de correo electrónico
-        emailjs.send('service_proof', 'template_reserve', paramsTuyo)
-            .then(function (response) {
-                console.log('Correo a tu dirección enviado con éxito:', response);
-                resetForm(); // Reiniciar el formulario después de enviar los correos
-            }, function (error) {
-                console.error('Error al enviar el correo a tu dirección:', error);
-            });
-    });
+        const sendYourEmail = emailjs.send('service_proof', 'template_reserve', paramsYours);
+
+       // Promesa para enviar ambos correos
+       Promise.all([sendClientEmail, sendYourEmail])
+       .then(function (responses) {
+           console.log('Correos enviados con éxito:', responses);
+           Swal.fire({
+               title: "Well done",
+               text: "successfully booked trip",
+               icon: "success",
+               allowOutsideClick: false, // Evitar que el usuario cierre el modal haciendo clic fuera de él
+               showCancelButton: false, // Ocultar el botón Cancelar
+               confirmButtonText: "OK", // Personalizar el texto del botón de confirmación
+           }).then(() => {
+               resetForm(); // Reiniciar el formulario después de enviar los correos
+               location.reload(); // Recargar la página
+           });
+       })
+       .catch(function (errors) {
+           console.error('Error al enviar los correos:', errors);
+           Swal.fire({
+               icon: "error",
+               title: "Oops...",
+               text: "Something went wrong! please try again.",
+             });
+       });
+});
 });
 
 function resetForm() {
-    document.getElementById("pickup-location-input").value = "";
-    document.getElementById("dropoff-location-input").value = "";
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("tel").value = "";
-    document.getElementById("pickUpDate").value = "";
-    document.getElementById("pick-up-time").value = "";
-    document.getElementById("additionalRemarks").value = "";
+document.getElementById('form').reset(); // Restablece todos los campos del formulario
 }
